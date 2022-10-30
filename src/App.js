3f8661f2473f6e0cebce799e2d84a2a2da74c37f
varshinichowdary27 <37653948+varshinichowdary27@users.noreply.github.com>
2022-10-30 11:06:31 -0400

    Update App.js
    "Changed the Overall look of signup and login"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import Home from './components/Home';
 import { BASE_ULR, URLS } from './config/constant';
 import SignInOutContainer from './containers/signInOutContainer';
-
+import { Grid, Paper,Box,CssBaseline } from '@material-ui/core'
+import arogya_poster from './arogya_poster.jpeg'
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(JSON.parse(sessionStorage.getItem("AUTH_TOKEN")) !== null);
@@ -25,15 +26,53 @@ const App = () => {
     return Promise.reject(error);
   });
   return (
+  
     <div className="App">
-     {loggedIn ? <Home loginCallBack={ () => {
+    <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+           <img src={arogya_poster} style={{width:'100%',height:'100%'}} alt="Arogya Image" />
+          </Grid>
+        
+
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          ></Box>
+          <div className='signinContainer' style={{width:'100%'}}>
+           {loggedIn ? <Home loginCallBack={ () => {
          setLoggedIn(sessionStorage.getItem("AUTH_TOKEN") !== null);
        }}/> : 
        <SignInOutContainer loginCallBack={ () => {
          setLoggedIn(sessionStorage.getItem("AUTH_TOKEN") !== null);
        }}></SignInOutContainer> 
-      }
-    </div>
+     
+     
+     }
+              </div>
+         </Grid> 
+         </Grid>
+      </div>
+
   );
 }
 
