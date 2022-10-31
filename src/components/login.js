620fb076a2cedfbaf0e1d365b59c60b9a02256dc
varshinichowdary27 <37653948+varshinichowdary27@users.noreply.github.com>
2022-10-30 22:45:19 -0400

    "Added account type to login"

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
-import { Grid, Paper, Avatar, TextField, Button, Typography, Link } from '@material-ui/core'
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link ,CssBaseline,Box} from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import { login } from '../services/loginAPI';
-
+import arogya_poster from '../arogya_poster.jpeg'
 const Login = ({ handleChange, loginCallBack}) => {
 
     const [errMsg, setErrMsg] = useState("");
+    const [accountType, setAccountType] = useState(10);
     const paperStyle = { padding: 20, height: '73vh', width: 300, margin: "0 auto" }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const btnstyle = { margin: '8px 0' }
+    const accountHandleChange = (event) =>{
+        setAccountType(event.target.value);
+            }
     const initialValues = {
         username: '',
         password: ''
@@ -38,6 +49,7 @@ const Login = ({ handleChange, loginCallBack}) => {
             }).finally(() =>  props.setSubmitting(false));
     }
     return (
+        
         <Grid>
             <Paper style={paperStyle} elevation={0}>
                 <Grid align='center'>
@@ -48,6 +60,21 @@ const Login = ({ handleChange, loginCallBack}) => {
                     {(props) => (
                         <Form>
                             {errMsg}
+                            <FormControl fullwidth >
+                                        <InputLabel id="demo-simple-select-label">Account Type</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={accountType}
+                                            label="Age"
+                                            onChange={accountHandleChange}
+                                        >
+                                            <MenuItem value={10}>Patient</MenuItem>
+                                           
+                                            
+                                            <MenuItem value={20}>manager</MenuItem>
+                                        </Select>
+                            </FormControl>
                             <Field as={TextField} label='Username' name="username"
                                 placeholder='Enter username' fullWidth required
                                 helperText={<ErrorMessage name="username" />}
@@ -68,6 +95,8 @@ const Login = ({ handleChange, loginCallBack}) => {
                 </Typography>
             </Paper>
         </Grid>
+        
+      
     )
 }
 
